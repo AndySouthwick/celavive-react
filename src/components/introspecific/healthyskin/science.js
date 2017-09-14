7a0f3fa53c864c387bd90a0dd8296d97c715f5d2
Andrew Southwick <asouthwick01@gmail.com>
2017-09-14 13:44:26 -0600

    styles

--- src/components/introspecific/healthyskin/science.js
+++ src/components/introspecific/healthyskin/science.js
@@ -8,13 +8,11 @@ class Scinece extends Component {
                 <Container>
                     <Row className="animatedRow">
                         <Col>
-                            <img src={require("../../img/bubbles-mobile.png")} className="ae7 mobile img-responsive" alt=""/>
                             <img src={require("../../img/celavive-pic-1.png")} alt="" className="celavive-pic-1 img-responsive desktop"/>
                             <img src={require("../../img/healthy-rect.png")} alt="" className="healthy-rect img-responsive desktop"/>
                             <img src={require("../../img/healthy-looking-skin.png")} alt="" className="healthy-quote img-responsive desktop"/>
                             <img src={require("../../img/healthy-mobile.png")} alt="" className="mobile img-responsive"/>
                         </Col>
-
                     </Row>
                 </Container>
             </section>
