fba322d0ba807fafd049308c3d808446ff088611
Andrew Southwick <asouthwick01@gmail.com>
2017-09-22 14:44:23 -0600

    close menu on click of link

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -60,7 +60,7 @@ class Navigation extends Component {
                 language: lan.language,
                 flag: lan.flag
               })}>
-                <Link to={`/${lan.language}/1`}><span className="flag"><img src={lan.flag} /></span> {lan.language}</Link>
+                <Link to={`/${lan.language}/1`} onClick={this.toggleMenuOff}><span className="flag"><img src={lan.flag} /></span> {lan.language}</Link>
               </div>
             </div>
           )
@@ -80,7 +80,7 @@ class Navigation extends Component {
                 language: lan.language,
                 flag: lan.flag
               })} >
-                <Link to={`/${lan.language}/1`}><span className="flag"><img src={lan.flag} /></span> {lan.language}  </Link>
+                <Link to={`/${lan.language}/1`} onClick={this.toggleMenuOff}><span className="flag"><img src={lan.flag} /></span> {lan.language}  </Link>
               </div>
             </div>
           )
@@ -101,7 +101,7 @@ class Navigation extends Component {
                 flag: lan.flag,
               })}>
                 {console.log(lan.flag)}
-                <Link to={`/${lan.language}/2/intro`}><span className="flag"><img src={lan.flag} /></span> {lan.language}</Link>
+                <Link to={`/${lan.language}/2/intro`} onClick={this.toggleMenuOff}><span className="flag"><img src={lan.flag} /></span> {lan.language}</Link>
               </div>
             </div>
           );
